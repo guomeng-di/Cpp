b4b29552c6eeb206dffabeacac11262c4baa0739
郭梦迪 <yjzty222@qq.com>
2026-01-16 15:20:23 +0800

    计算摄氏温度

---
+++ 04.cpp
@@ -0,0 +1,11 @@
+//计算摄氏温度
+#include <iostream>
+using namespace std;
+int main(){
+    int F;
+    cin>>F;
+    int C=5*(F-32)/9;
+    cout<<"Celsius = "<<C<<endl;
+    system("pause");
+    return 0;
+}
