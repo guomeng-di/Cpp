e618883b88d1d61b46f740eb815cf190f53275d3
郭梦迪 <yjzty222@qq.com>
2026-01-16 15:36:23 +0800

    整数四则运算

---
+++ 05.cpp
@@ -0,0 +1,13 @@
+//整数四则运算
+#include <iostream>
+using namespace std;
+int main(){
+    int A,B;
+    cin>>A>>B;
+    cout<<A<<" + "<<B<<" = "<<A+B<<endl;
+    cout<<A<<" - "<<B<<" = "<<A-B<<endl;
+    cout<<A<<" * "<<B<<" = "<<A*B<<endl;
+    cout<<A<<" / "<<B<<" = "<<A/B<<endl;
+    system("pause");
+    return 0;
+}
