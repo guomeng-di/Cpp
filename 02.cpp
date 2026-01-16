c8860e48e6f19fbf23280cabf4524fbe3d4c3147
郭梦迪 <yjzty222@qq.com>
2026-01-16 14:32:57 +0800

    温度转换

---
+++ 02.cpp
@@ -0,0 +1,11 @@
+//温度转换
+#include <iostream>
+using namespace std;
+int main(){
+    int F=150;
+    int C=5*(F-32)/9;
+    cout<<"fahr = 150, celsius = "<<C<<endl;
+    system("pause");
+    return 0;
+}
+
