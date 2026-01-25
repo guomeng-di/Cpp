22db455abb6c2dd6a2802449d3024a139077dff8
郭梦迪 <yjzty222@qq.com>
2026-01-25 18:16:30 +0800

    43919. 神秘的三位数

---
+++ prac12.cpp
@@ -0,0 +1,24 @@
+//43919. 神秘的三位数
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+int JC[] = {1,1,2,6,24,120,720,5040,40320,362880};
+    int i;
+    for(i=100; i<1000; i++)
+    {
+        int sum = 0;
+        int x = i;
+        while(x)
+        {
+            sum += JC[x%10];
+            x /= 10;
+        }
+        if(i==sum) printf("%d\n", i);
+    }
+
+
+  return 0;
+}
